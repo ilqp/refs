f5383c6e13602b129a3ca6704377f3521f5ee56c
user <user@users-MacBook-Pro.local>
2018-06-11 21:39:17 +0530

    Adding brew setup

---
+++ init.sh
@@ -0,0 +1,3 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew cask install sublime-text cmake firefox beyond-compare telegram whatsapp vmware-fusion db-browser-for-sqlite microsoft-office kindle gitkraken # vlc transmission
+brew install git
