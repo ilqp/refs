33b4f1ab0401fd82ff42dba77432922b27c5978d
user <user@users-MacBook-Pro.local>
2018-06-11 21:45:53 +0530

    Updates to brew setup

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew cask install sublime-text cmake firefox beyond-compare telegram whatsapp vmware-fusion db-browser-for-sqlite microsoft-office kindle gitkraken # vlc transmission
 brew install git
+# curl https://github.com/TheTorProject/gettorbrowser/releases/download/v7.5.3/TorBrowser-7.5.3-osx64_en-US.dmg -o /tmp/TorBrowser-7.5.3-osx64_en-US.dmg
